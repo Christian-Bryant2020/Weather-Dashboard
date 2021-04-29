27244a4cc161ca99a7b3fb5958bdd52833d16e0a
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-04-29 12:33:26 -0500

    Updates to display
    Added wind speed, temp, and humidity to the display.

--- index.js
+++ index.js
@@ -1,50 +1,42 @@
 var btn = document.getElementById("search-input");
 const apiKey = 'd266a33294e8e5458622d88908376ab6';
+let temp = document.getElementById('temp');
+let humidity = document.getElementById('humid');
+let wind = document.getElementById('wind');
+let uv = document.getElementById('uv');
+let dayOne = document.getElementById('dayOne');
 
 
-
-let weatherSearch = function(event) {
+let weatherSearch = function (event) {
     var cityInput = document.getElementById('citySearch');
     inputValue = cityInput.value.trim();
-    let apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + inputValue + '&appid='+ apiKey;
-console.log(apiUrl)
-    localStorage.setItem('City',inputValue);
+    let apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + inputValue + '&appid=' + apiKey + '&units=imperial';
+    localStorage.setItem('City', inputValue);
     let recentEntry = localStorage.getItem('City');
-    document.getElementById('location').textContent = "Search results for: " + recentEntry;
-    
+    document.getElementById('location').textContent = recentEntry;
+    document.getElementById('cityName').textContent = recentEntry + ' (date)'
+    fetch(apiUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var tempValue = data['main']['temp'];
+            var humidValue = data['main']['humidity'];
+            var windValue = data['wind']['speed'];
+            var uvValue = data['main']['temp'];
+
+            document.getElementById('temp').textContent = tempValue;
+            console.log(data);
+            document.getElementById('humid').textContent = humidValue;
+            document.getElementById('wind').textContent = windValue;
+            document.getElementById('uv').textContent = uvValue;
+
+
+        })
+        // .catch(funcion(err){
+        //     alet('Invalid input')
+        // })
+
+
 }
-
-btn.onclick= weatherSearch;
-
-
-
-
-
-
-
-//api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}
-
-
-
-// var	formSubmitHandler = function(event) {
-// 	console.log("formSubmitHandler is fine");
-// 	event.preventDefault();
-
-//     var userInput = document.getElementById("search-input");
-// 	inputValue = userInput.value.trim();
-	
-// 		getApi(inputValue);
-// 		localStorage.setItem("city", inputValue);
-// 		var recentEntry = localStorage.getItem("city");
-// 		document.getElementById("searchResults").textContent = "Search results for: " + recentEntry;
-// 	}
-	
-// let getApi = function () {
-//     fetch(apiUrl)
-//     .then(function(response){
-//         return response.json();
-//     })
-//     .then(function (data) {
-// console.log(data);
-//     })
-// }
+btn.onclick = weatherSearch;
